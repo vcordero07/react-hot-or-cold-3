3577498b498bbdfedb6d2c951521e77e2aed050d
vcordero07 <vcordero07@gmail.com>
2018-01-18 23:05:32 -0500

    wip

--- src/components/test/guess-count.test.js
+++ src/components/test/guess-count.test.js
@@ -7,4 +7,11 @@ describe('<GuessCount />', () => {
   it('Renders without crashing', () => {
     shallow(<GuessCount />);
   });
+
+  it('Should render the GuessCount and return exact guessCount', () => {
+    const _guessCount = 11;
+    const _guessCountTxt = `You've made ${_guessCount} guesses!`
+    const wrapper = shallow(<GuessCount guessCount={_guessCount} />);
+    expect(wrapper.text()).toEqual(_guessCountTxt);
+  });
 })
--- src/components/test/guess-list.test.js
+++ src/components/test/guess-list.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import GuessList from '../guess-list';
-let guesses = [1,2,3,4,5];
+
 
 describe('<GuessList />', () => {
   it('Should render GuessList given an Array{guesses}', () => {
+    const guesses = [1,2,3,5,8,13,21];
     shallow(<GuessList guesses={guesses}/>);
+  });
+
+  it('Should return an Array list of gueeses', () => {
+    const guesses = [1,2,3,5,8,13,21];
+    const _nGuesses = guesses.join('');
+    const wrapper = shallow(<GuessList guesses={guesses} />);
+    expect(wrapper.text()).toEqual(_nGuesses);
   });
 })
